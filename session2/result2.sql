d6eaa36a9c1369eb6c23dc8481196f26bf739c18
GerwinP <gerwinputtenstein@gmail.com>
2014-05-06 15:56:43 +0200

    Task b

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,4 +1,7 @@
---Task a)
+--- Task a ---
 INSERT INTO F (x, y, violation, motivation) VALUES('ABC', 'EF', 0, 'Als je ABC weet, weet je E, en als je E weet, weet je D');
 INSERT INTO F (x, y, violation, motivation) VALUES('E', 'D', 1, 'Als je E weet, weet je alleen D en A, maar niet de rest');
 INSERT INTO F (x, y, violation, motivation) VALUES('D', 'A', 1, 'Als je D weet, weet je alleen A');
+--- Task b ---
+INSERT INTO F1(x, y, violation, motivation) VALUES('E', 'D', 0, 'Er zit verder niks in, dus het is geen violation');
+INSERT INTO F2(x, y, violation, motivation) VALUES('ABC', 'EF', 0, 'Dit was al geen violation');
